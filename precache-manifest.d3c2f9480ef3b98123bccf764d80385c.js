c2a9760454a1f2e05cdf917572040b91dfbfb212
Binh Phan <binh.phan@gmail.com>
2020-01-27 23:13:22 -0800

    update

---
+++ precache-manifest.d3c2f9480ef3b98123bccf764d80385c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e9dd33bed36785cabeb",
+    "url": "/static/js/main.0e9dd33b.chunk.js"
+  },
+  {
+    "revision": "23570ef3aff95e149964",
+    "url": "/static/js/1.23570ef3.chunk.js"
+  },
+  {
+    "revision": "0e9dd33bed36785cabeb",
+    "url": "/static/css/main.9ed0a468.chunk.css"
+  },
+  {
+    "revision": "23570ef3aff95e149964",
+    "url": "/static/css/1.427ec374.chunk.css"
+  },
+  {
+    "revision": "de32ebdb16f7692d0761d7d6bf63b618",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04ebe79e2d101b71fa17516179bb72b2.js"
+  "/precache-manifest.d3c2f9480ef3b98123bccf764d80385c.js"
 );
 
 workbox.clientsClaim();
